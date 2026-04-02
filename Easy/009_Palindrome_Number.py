6ab68d9633897b34b2294fc72caeda241602f240
Jo-9m-n1 <leejongmin1092@gmail.com>
2026-04-02 11:18:44 -0400

    feat: add solution for LeetCode #13: Roman to Integer (Python)

--- Easy/009_Palindrome_Number.py
+++ Easy/009_Palindrome_Number.py
@@ -30,6 +30,7 @@ class Solution:
             return False
         
         reversed_half = 0
+        
         while x > reversed_half:
             last_digit = x % 10
             reversed_half = (reversed_half * 10) + last_digit
